b3f973232cdcb3f8e2c3abd52ece4df62c152b8c
henriqueLebre <henrique_lebre@hotmail.com>
2023-09-14 21:04:25 -0300

    CRUD Repository Role

--- Repositories/RoleRepository.cs
+++ Repositories/RoleRepository.cs
@@ -18,4 +18,23 @@ public class RoleRepository
         => _connection.Get<Role>(id); // Retornando com Expression Body
     public void Create(Role role)
         => _connection.Insert<Role>(role); // Retornando com Expression Body
+
+    public void Update(Role role)
+    {
+        if (role.Id != 0)
+            _connection.Update<Role>(role);
+    }
+    public void Delete(Role role)
+    {
+        if (role.Id != 0)
+            _connection.Delete<Role>(role);
+    }
+    public void Delete(int id)
+    {
+        if (id != 0)
+            return;
+
+        var role = _connection.Get<Role>(id);
+        _connection.Delete<Role>(role);
+    }
 }
